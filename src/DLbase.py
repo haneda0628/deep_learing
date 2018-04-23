372578550b04406515c00c0e0460f79464db0842
haneda0628 <m52025.xof@gmail.com>
2018-04-24 05:37:01 +0900

    #0002 2018/4/24 test affine func

--- src/DLbase.py
+++ src/DLbase.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 class MulLayer:
     def __init__(self):
@@ -37,7 +38,7 @@ class AffineLayer:
         self.b = b
         self.x = None
 
-    def forward(self.x):
+    def forward(self, x):
         self.x = x
         out = np.dot(x, self.W) + self.b
         return out
@@ -46,11 +47,52 @@ class AffineLayer:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis = 0)
-
         return dx
 
-addLayer = MulLayer()
-v = addLayer.forward(3,4)
-print v
-dv = addLayer.backward(10)
-print dv
+class ReLULayer:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = copy.copy(x)
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+def test_addLayer():
+    addLayer = MulLayer()
+    v = addLayer.forward(3,4)
+    print v
+    dv = addLayer.backward(10)
+    print dv
+
+def test_ReLULayer():
+    layer = ReLULayer()
+    y = layer.forward([-3,4])
+    print y
+    dx = layer.backward([10,3])
+    print dx
+
+def test_AffineLayer():
+    layer = AffineLayer(
+    np.array(
+    [
+        [2,2,4],
+        [3,4,2],
+        [5,1,3]
+    ]),
+        np.array([3,4,2])
+
+    )
+
+    y = layer.forward(np.array([2,3,4]))
+    print y
+    dx = layer.backward(np.array([8,9,6]))
+    print dx
+
+test_AffineLayer()
